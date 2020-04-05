9cbffb75d8373f7380bd55e87287623f9e7618c3
Jason Lin <>
2020-04-05 20:13:21 +0800

    47 行沒有功能

--- heartPoker/PlayGameViewController.swift
+++ heartPoker/PlayGameViewController.swift
@@ -22,6 +22,7 @@ class PlayGameViewController: UIViewController {
     
     let suits = ["♠️", "♥️", "♦️", "♣️"]
     let ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    var pokerCard:[Card] = []
     var gameTimer:Timer?
     var judgeWinner:Bool = false // 是否判斷勝負
     var showPokerNumber:Int = 0 // 牌組中第幾張牌
@@ -31,27 +32,24 @@ class PlayGameViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.handImage()
-        
-//        self.gameInitial()
-        self.gameStart()
-
-        
-//        while judgeWinner == false{
-            // 延遲執行
-//            DispatchQueue.main.asyncAfter(deadline: .now() + Double.random(in: 0.4...1.2)) {
-//                self.gameStart()
-//            }
-//        }
-
-        
-    }
+        // 創建遊戲牌組
+        pokerCard = pokerList()
+        self.hitNumber = self.ranks
     
-    // 創建手掌圖片
-    func handImage()->(){
-        // 電腦的手旋轉180度
-        computerHand?.transform = CGAffineTransform(scaleX: 1, y: -1)
+        self.gameInitial()
+//        self.gameStart()
+
+        
+        while judgeWinner == false{
+            print("1")
+            // 延遲執行
+            DispatchQueue.main.asyncAfter(deadline: .now() + Double.random(in: 0.4...1.2)) {
+                print("2")
+                self.gameStart()
+            }
+        }
+
+        
     }
     
     // 點擊螢幕觸發
@@ -69,12 +67,15 @@ class PlayGameViewController: UIViewController {
     // 遊戲初始化
     func gameInitial(){
         // 牌組亂序
-        var pokerCard = pokerList()
         pokerCard.shuffle()
         // 數值歸零
         self.judgeWinner = false
         self.gameNumber = 0
         self.showPokerNumber = 0
+        // 手掌圖片歸位
+        computerHand?.transform = CGAffineTransform.identity.scaledBy(x: 1, y: -1)
+        //self.computerHand?.transform = CGAffineTransform(scaleX: 1, y: -1)
+        userHand?.transform = CGAffineTransform.identity
     }
     
     // 產生52張撲克牌
@@ -90,35 +91,28 @@ class PlayGameViewController: UIViewController {
     
     // 遊戲開始
     func gameStart() -> (){
-        var pokerCard = self.pokerList()
-        pokerCard.shuffle()
-        self.hitNumber = self.ranks
-        
-        
-        self.gameTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (_) in
+//        self.gameTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (_) in
             
             // 判斷是否中獎
-            if  self.hitNumber[self.gameNumber] == pokerCard[self.showPokerNumber].rank{
+            if  self.hitNumber[self.gameNumber] == self.pokerCard[self.showPokerNumber].rank{
                 // 時間停止
                 self.gameTimer?.invalidate()
                 self.judgeWinner = true
                 
                 // 電腦隨機秒數按下
                 DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + Double.random(in: 0.4...0.8)) {
-                    self.computerHand?.transform = CGAffineTransform(translationX: 0, y:400)
-                    self.computerHand?.transform = CGAffineTransform(scaleX: 1, y: -1)
                     self.battle("AIWin")
                 }
             }
             // 顯示目前的牌及遊戲數字
-            self.pokerImageView.image = UIImage(named: "\(pokerCard[self.showPokerNumber].suit + pokerCard[self.showPokerNumber].rank)")
+            self.pokerImageView.image = UIImage(named: "\(self.pokerCard[self.showPokerNumber].suit + self.pokerCard[self.showPokerNumber].rank)")
             self.showGameNumber.text = "\(self.hitNumber[self.gameNumber])"
             
             // 牌組的牌使用完則重新亂數並歸零
             self.showPokerNumber += 1
             if self.showPokerNumber >= 52{
                 self.showPokerNumber = 0
-                pokerCard.shuffle()
+                self.pokerCard.shuffle()
             }
             
             // 使遊戲中獎的數字為1~13循環
@@ -126,7 +120,7 @@ class PlayGameViewController: UIViewController {
             if self.gameNumber >= 13{
                 self.gameNumber = 0
             }
-        })
+//        })
     }
     
     
@@ -140,6 +134,7 @@ class PlayGameViewController: UIViewController {
             case "gamerWin":
                 gameOverAlertTitle = "You are the winner!"
             case "AIWin":
+                self.computerHand?.transform = CGAffineTransform(translationX: 0, y:400).scaledBy(x: 1, y: -1)
                 gameOverAlertTitle = "You are the loser!"
             default:
                 print("error")
